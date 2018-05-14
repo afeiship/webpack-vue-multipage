9659f0fedf7fd6920a1d5f31227b1285c5b0666c
afeiship <1290657123@qq.com>
2018-05-14 22:18:29 +0800

    issue: optmize deploy script

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -3,11 +3,9 @@ const merge = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.base.conf');
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
 
-const prodWebpackConfig = merge(baseWebpackConfig, {
+module.exports = merge(baseWebpackConfig, {
   devtool: '#source-map',
   plugins: [
     new OptimizeCSSPlugin()
   ]
 });
-
-module.exports = prodWebpackConfig;
