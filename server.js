87ef72488c6054645e570afc77bec117021f870e
afeiship <1290657123@qq.com>
2018-05-15 08:16:58 +0800

    issue: optize server port

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const app = express();
 
-app.use(express.static('./dist'))
+app.use(express.static('./dist'));
 
 app.get('/', function (req, res) {
   res.send('Hello Vue');
-})
+});
 
-app.listen(2333)
+app.listen(2333);
