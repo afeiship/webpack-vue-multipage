189b88547b60aea23bbaeb60e7b8e7d51d15f5c9
afeiship <1290657123@qq.com>
2018-05-30 17:53:53 +0800

    issue: basic vuex

--- src/pages/home/app.js
+++ src/pages/home/app.js
@@ -1,9 +1,18 @@
 import Vue from 'vue';
 import App from './app.vue';
 import 'assets/styles/index.scss';
+import vuex from 'vuex';
 
+Vue.use(vuex);
+
+const store = new vuex.Store({//store对象
+  state: {
+    show: false
+  }
+});
 
 new Vue({
   el: '#app',
+  store,
   render: h => h(App)
 });
--- src/pages/home/app.vue
+++ src/pages/home/app.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="home-view" class="auto tc wp-7">
-        <div class="dd">
-            Home
+        <div class="dd" style="color:#333;">
+            Home - show: {{ show }}
         </div>
         <img v-bind:src="logoImg" class="wp-2 auto logo">
         <header>
@@ -17,10 +17,12 @@
 <script>
   import logo from 'assets/images/logo.png';
   import {$api} from 'components';
+  import {mapState} from 'vuex';
 
   export default {
     data() {
       return {
+        ...this.$store.state,
         msg: 'Use Vue 2.0 Today!',
         logoImg: logo
       }
@@ -40,8 +42,8 @@
           password: '123123'
         }).then(resp => {
           console.log('resp');
+          this.show = !this.show;
         });
-        console.log('click!');
       }
     }
   }
